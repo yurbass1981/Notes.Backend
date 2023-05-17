81d664a54af7c103c0801810b2bcbdc0b1745095
Yuri Ilyushin <yurbass1@rambler.ru>
2023-05-17 19:19:40 +0300

    Added entity type

---
+++ Notes.Persistens/EntityTypeConfigurations/NoteConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Notes.Persistens.EntityTypeConfigurations
+{
+    internal class NoteConfiguration : IEntityTypeConfiguration<Note>
+    {
+    }
+}
