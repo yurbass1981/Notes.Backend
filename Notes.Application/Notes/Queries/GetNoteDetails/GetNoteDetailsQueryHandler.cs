73a6f55d659fee1412ce8bf26273dd4c8e559387
Yuri Ilyushin <yurbass1@rambler.ru>
2023-06-28 16:38:30 +0300

    Added new classes

---
+++ Notes.Application/Notes/Queries/GetNoteDetails/GetNoteDetailsQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Notes.Application.Common.Exceptions;
+using Notes.Application.Interfaces;
+using Notes.Domain;
+
+namespace Notes.Application.Notes.Queries.GetNoteDetails
+{
+    public class GetNoteDetailsQueryHandler
+        : IRequestHandler<GetNoteDetailsQuery, NoteDetailsVm>
+    {
+        private readonly INotesDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetNoteDetailsQueryHandler(INotesDbContext dbContext,
+            IMapper mapper) => (_dbContext, _mapper) = (dbContext, mapper);
+        
+        public async Task<NoteDetailsVm> Handle(GetNoteDetailsQuery request,
+            CancellationToken cancellationToken)
+        {
+            var entity = await _dbContext.Notes
+                .FirstOrDefaultAsync(note => note.Id == request.Id, cancellationToken);
+
+            if (entity == null || entity.Id != request.UserId) 
+            {
+                throw new NotFoundException(nameof(Note), request.Id);
+            }
+
+            return _mapper.Map<NoteDetailsVm>(entity);
+        }
+    }
+}
---
+++ Notes.Application/Notes/Queries/GetNoteList/NoteListVm.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Notes.Application.Notes.Queries.GetNoteList
+{
+    public class NoteListVm
+    {
+        public IList<NoteLookupDto> Notes { get; set; }
+    }
+}
